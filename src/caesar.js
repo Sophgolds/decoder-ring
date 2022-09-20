933b248287baf0c9d4f46cbe809f5b26cfab6979
Sophie Goldstein <110262769+Sophgolds@users.noreply.github.com>
2022-09-20 17:16:05 -0400

    Update caesar.js
    solution updated

--- src/caesar.js
+++ src/caesar.js
@@ -1,18 +1,33 @@
-// Please refrain from tampering with the setup code provided here,
-// as the index.html and test files rely on this setup to work properly.
-// Only add code (helper methods, variables, etc.) within the scope
-// of the anonymous function on line 6
+const substitutionModule = (function () {
 
-const caesarModule = (function () {
-  // you can add any code you want within this function scope
+  const abc = "abcdefghijklmnopqrstuvwxyz".split("");
+  const decoder = [];
+  function substitution(input, alphabet, encode = true) {
 
-  function caesar(input, shift, encode = true) {
-    // your solution code here
+    const noRepeats = new Set(alphabet);
+    if (alphabet === undefined || alphabet.length < 26 || [...noRepeats].length < 26) return false;
+        
+    alphabet.split("");
+    if (encode){
+      for (let i = 0; i < 26; i++)
+      {decoder[abc[i]] = alphabet[i]};
+    } 
+    else
+    {for (let i = 0; i < 26; i++)
+      {decoder[alphabet[i]] = abc[i]};
+    }
+    
+    let result = input.toLowerCase().split("").map((letter) => {
+      if (letter === " ") return " ";
+      return decoder[letter]
+    })
+    return result.join("");
   }
 
   return {
-    caesar,
+    substitution,
   };
 })();
 
-module.exports = { caesar: caesarModule.caesar };
+module.exports = { substitution: substitutionModule.substitution };
+
